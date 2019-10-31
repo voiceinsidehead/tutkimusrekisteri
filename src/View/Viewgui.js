6cceea0a5d607426fd35ca2088d0cb7391f2b4df
MatiasPuputti <matias.v.puputti@student.samk.fi>
2019-10-31 09:42:32 +0200

    Added table functionality

--- src/View/Viewgui.js
+++ src/View/Viewgui.js
@@ -1,12 +1,17 @@
 window.onload = function() {
-  //dragElement(document.getElementById("mydiv"));
+  dragElement(document.getElementById("demoTable"));
   document.getElementById("Back").addEventListener("click", BackPage);
 
   Tab_Selected = document.getElementById("tab_Sel");
   Tab_Selected.addEventListener("change", Buttongreen);
   Submit.addEventListener("click", Buttongreen);
+  window.addEventListener("keyup", tableControl);
+  window.addEventListener("keypress", tableControl);
+  window.addEventListener("keydown", tableControl);
+  window.addEventListener("wheel", tableControl);
 };
 
+dragElement(document.getElementById("demoTable"));
 //Function for deciding wich table to show and button (Id == Submit) color.
 function Buttongreen(event) {
   var selected = document.getElementById("tab_Sel").value;
@@ -17,6 +22,7 @@ function Buttongreen(event) {
     case selected == "Default":
       submit.style.background = "white";
       submit.style.color = "black";
+      document.getElementById("demoTable").style.visibility = "hidden";
       break;
 
     //Changes color of the button to green to symbolize that selections are aproved.
@@ -27,7 +33,7 @@ function Buttongreen(event) {
 
     //Case when selection is specified and specific button (Id == Submit) is pressed.
     case selected == "Person" && event.type == "click":
-      Showtable("Demo Person");
+      document.getElementById("demoTable").style.visibility = "visible";
       //Function for pulling up a table here.
       break;
 
@@ -39,8 +45,76 @@ function Buttongreen(event) {
 
     //Case when selection is specified and specific button (Id == Submit) is pressed.
     case selected == "ResearchId" && event.type == "click":
-      Showtable("Demo ResearchId");
+      document.getElementById("demoTable").style.visibility = "visible";
       //Function for pulling up a table here.
+      break;
+  }
+}
+
+var direction = 0;
+var scaleValue = 1;
+var map = {};
+
+function tableControl(e) {
+  e = e || event; // to deal with IE
+  map[e.keyCode] = e.type == "keydown";
+  var demoTable = document.getElementById("demoTable");
+  var windowWidht = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  var leftValue;
+  var topValue;
+
+  switch (true) {
+    //Detect moesewheel direction.
+    case e.deltaY < 0:
+      direction = 1;
+      break;
+
+    //Detect moesewheel direction.
+    case e.deltaY > 0:
+      direction = -1;
+      break;
+
+    //"Zoom" table.
+    case (map[17] && map[107]) || direction > 0:
+      direction = 0;
+      scaleValue += 0.1;
+      document.getElementById("demoTable").style.transform =
+        "scale(" + scaleValue + ")";
+      break;
+
+    //"Zoom" table.
+    case (map[17] && map[109]) || direction < 0:
+      direction = 0;
+      scaleValue -= 0.1;
+      document.getElementById("demoTable").style.transform =
+        "scale(" + scaleValue + ")";
+      break;
+
+    //Make table move to left.
+    case map[17] && map[37]:
+      leftValue = (demoTable.offsetLeft / windowWidht) * 100 - 0.5;
+      document.getElementById("demoTable").style.left = leftValue + "%";
+      break;
+
+    //Make table move to right.
+    case map[17] && map[39]:
+      leftValue = (demoTable.offsetLeft / windowWidht) * 100 + 0.5;
+      document.getElementById("demoTable").style.left = leftValue + "%";
+      break;
+
+    //Make table move up.
+    case map[17] && map[38]:
+      topValue = (demoTable.offsetTop / windowHeight) * 100 - 0.5;
+      document.getElementById("demoTable").style.top = topValue + "%";
+      break;
+
+    case map[17] && map[40]:
+      topValue = (demoTable.offsetTop / windowHeight) * 100 + 0.5;
+      document.getElementById("demoTable").style.top = topValue + "%";
+      break;
+
+    default:
       break;
   }
 }
@@ -58,7 +132,21 @@ function BackPage() {
 
 function ViewPage() {
   location.replace("View/View.html");
-} /*
+}
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + "demoTable")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "demoTable").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
@@ -68,7 +156,7 @@ function ViewPage() {
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
-  }*/ /*
+  }
 
   function elementDrag(e) {
     e = e || window.event;
@@ -82,24 +170,10 @@ function ViewPage() {
     elmnt.style.top = elmnt.offsetTop - pos2 + "px";
     elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
   }
-*/ /*
+
   function closeDragElement() {
     // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
- */
-/*
-function dragElement(elmnt) {
-  var pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
-    pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-*/
+}
